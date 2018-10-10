ae2c2581fe51035c36e120cf837f56464d16e4e7
Adam Cowdy <cowdyadam@gmail.com>
2018-10-10 17:26:24 +0100

    Improve readability of setup file

--- setup.py
+++ setup.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python
 
+import os
 from setuptools import setup, find_packages
 from rocheck import VERSION
+
+
+def read_file(path):
+    return open(os.path.join(os.path.dirname(__file__), path)).read()
+
 
 setup(
     name='ro-check',
@@ -9,7 +15,7 @@ setup(
     packages=find_packages(),
     license='Apache Licence 2',
     description='tool for validating research objects',
-    long_description=open('README.md').read(),
+    long_description=read_file('README.md'),
     entry_points={
         'console_scripts': ['ro-check=rocheck.cli:main'],
     }
