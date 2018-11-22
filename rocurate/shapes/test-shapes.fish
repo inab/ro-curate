0e8612fc4044b7496d8334522fb09613f15c29e8
Adam Cowdy <cowdyadam@gmail.com>
2018-11-22 22:40:01 +0000

    Create full shapes file and tests

--- rocurate/shapes/test-shapes.fish
+++ rocurate/shapes/test-shapes.fish
@@ -4,3 +4,4 @@ assert -- pyshacl -s shapes.ttl prefixes.ttl
 and assert -- not pyshacl -s shapes.ttl blankro.ttl
 and assert -- pyshacl -s shapes.ttl minimalro.ttl
 and assert -- pyshacl -s shapes.ttl ro.ttl
+and assert -- not pyshacl -s shapes.ttl ro-noprov.ttl
