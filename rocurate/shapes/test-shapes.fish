2654f1ced72f902053796b60fe04be8d8722303e
Adam Cowdy <cowdyadam@gmail.com>
2018-11-22 20:18:09 +0000

    Create shapes for a simple research object (without annotations)

--- rocurate/shapes/test-shapes.fish
+++ rocurate/shapes/test-shapes.fish
@@ -1,5 +1,6 @@
 #!/usr/bin/env fish
 
 assert -- pyshacl -s shapes.ttl prefixes.ttl
-assert -- not pyshacl -s shapes.ttl blankro.ttl
-assert -- pyshacl -s shapes.ttl minimalro.ttl
+and assert -- not pyshacl -s shapes.ttl blankro.ttl
+and assert -- pyshacl -s shapes.ttl minimalro.ttl
+and assert -- pyshacl -s shapes.ttl ro.ttl
